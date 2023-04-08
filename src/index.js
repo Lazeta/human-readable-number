830a15f1e217110edfe4e360c8bfda62cbca61c4
lazeta <jcuccllhfppyxoyddfdlhbh@gmail.com>
2023-04-08 20:59:37 +0300

    refactor: update code for miniatures

--- src/index.js
+++ src/index.js
@@ -1,44 +1,30 @@
-module.exports = function toReadable(numbers) {
-    let array_1 = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine',
-        'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen',];
-    let array_2 = ['hundred', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety',];
+module.exports = function toReadable(num) {
+    const arr_1 = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 
+    'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen',];
+    const arr_2 = ['hundred', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety',];
 
-    // numbers from 1 to 19
-    if (numbers >= 0 && numbers < 20) {
-        return array_1[numbers];
+    // num from 1 to 19
+    if (num >= 0 && num < 20) return arr_1[num];
+    
+    // num from 20 to 99
+    if (num >= 20 && num < 100) {
+        let b_1 = Math.floor(num / 10);
+
+        if (num % 10 != 0) return `${arr_2[b_1]} ${arr_1[num % 10]}`;
+        else return arr_2[b_1];
     }
-
-    // numbers from 20 to 99
-    if (numbers >= 20 && numbers < 100) {
-        let b_1 = Math.floor(numbers / 10);
-        if (numbers % 10 != 0) {
-            return `${array_2[b_1]} ${array_1[numbers % 10]}`;
-        }
-        else {
-            return array_2[b_1];
-        }
-    } // numbers from 99 to 999
-    else if (numbers > 99 && numbers < 1000) {
-        let b_1 = Math.floor(numbers / 100);
-        let b_2 = Math.floor(numbers % 100 / 10);
-        let b_3 = numbers % 10;
+    // num from 99 to 999
+    else if (num > 99 && num < 1000) {
+        let b_1 = Math.floor(num / 100);
+        let b_2 = Math.floor(num % 100 / 10);
+        let b_3 = num % 10;
 
         if (b_2 != 0 && b_3 != 0) {
-            if (b_2 == 1) {
-                return `${array_1[b_1]} ${array_2[0]} ${array_1[[b_3 + 10]]}`;
-            }
-            else {
-                return `${array_1[b_1]} ${array_2[0]} ${array_2[b_2]} ${array_1[b_3]}`;
-            };
+            if (b_2 == 1) return `${arr_1[b_1]} ${arr_2[0]} ${arr_1[[b_3 + 10]]}`;
+            else return `${arr_1[b_1]} ${arr_2[0]} ${arr_2[b_2]} ${arr_1[b_3]}`;
         };
-        if (b_2 == 0 && b_3 != 0) { 
-            return `${array_1[b_1]} ${array_2[0]} ${array_1[b_3]}` 
-        };
-        if (b_2 != 0 && b_3 == 0) { 
-            return `${array_1[b_1]} ${array_2[0]} ${array_2[b_2]}` 
-        };
-        if (b_2 == 0 && b_3 == 0) { 
-            return `${array_1[b_1]} ${array_2[0]}` 
-        };
+        if (b_2 == 0 && b_3 != 0) return `${arr_1[b_1]} ${arr_2[0]} ${arr_1[b_3]}`;
+        if (b_2 != 0 && b_3 == 0) return `${arr_1[b_1]} ${arr_2[0]} ${arr_2[b_2]}`;
+        if (b_2 == 0 && b_3 == 0) return `${arr_1[b_1]} ${arr_2[0]}`;
     }
 }
